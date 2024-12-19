800cade6feb86da165f3ed339eeed4de18b149eb
UPENDRA\upend <upen28.verma@gmail.com>
2024-12-19 22:14:51 +0530

    add test

---
+++ src/test/java/com/polarbookshop/orderservice/order/web/BookTest.java
@@ -0,0 +1,29 @@
+package com.polarbookshop.orderservice.order.web;
+
+import org.junit.Test;
+import org.mockito.Mock;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import com.polarbookshop.orderservice.order.domain.OrderService;
+
+@WebMvcTest(OrderController.class)
+public class BookTest {
+
+	@Mock
+	private OrderService orderService;
+
+	@Test
+	public void testBook() {
+
+	}
+
+	@Test
+	public void testBook2() {
+
+	}
+
+	@Test
+	public void testBook3() {
+
+	}
+
+}
